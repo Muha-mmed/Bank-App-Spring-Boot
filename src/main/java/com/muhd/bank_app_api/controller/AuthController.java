259ea7487d9228eb686e1a84cc7cf6be034ce082
Muhammed <your.mrobiu001@example.com>
2025-10-17 09:07:35 -0700

    successfully adding jwt, generation token and creating transactions

--- src/main/java/com/muhd/bank_app_api/controller/AuthController.java
+++ src/main/java/com/muhd/bank_app_api/controller/AuthController.java
@@ -1,12 +1,8 @@
 package com.muhd.bank_app_api.controller;
 
 import com.muhd.bank_app_api.model.BankUser;
-import com.muhd.bank_app_api.security.JwtUtil;
 import com.muhd.bank_app_api.service.serviceImplementation.AuthServiceImple;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
